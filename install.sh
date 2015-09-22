00d7cf1fb178efe84242cd16a2bbe6363087790d
pydog <wuxinxinggg@qq.com>
2015-09-22 23:10:35 +0800

    update install script

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-if [ -d $HOME/.vim ];then
-    echo ".vim directory exist, rename to .vim_bak"
-    mv $HOME/.vim $HOME/.vim_bak
+#if [ 0 -eq 1 ];then
+if [ -L $HOME/.vim ];then
+    echo ".vim directory exist, delete .vim_bak and relink"
+    rm -fr $HOME/.vim
     ln -s $HOME/neovim-config/ $HOME/.vim
 else
     echo "install .vim"
@@ -13,30 +14,30 @@ echo "install plug.vim"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-if [ -f $HOME/.vimrc ];then
-    echo ".vimrc exist, rename to .vimrc.bak"
-    mv $HOME/.vimrc $HOME/.vimrc.bak
+if [ -L $HOME/.vimrc ];then
+    echo ".vimrc exist, delete .vimrc.bak and relink"
+    rm -fr $HOME/.vimrc
     ln -s $HOME/.vim/nvimrc $HOME/.vimrc
 else
     echo "install .vimrc"
     ln -s $HOME/.vim/nvimrc $HOME/.vimrc
 fi
 
-if [ -f $HOME/.tmux.conf ];then
-    echo ".tmux.conf exist, rename to .tmux.conf.bak"
-    mv $HOME/.tmux.conf $HOME/.tmux.conf.bak
+if [ -L $HOME/.tmux.conf ];then
+    echo ".tmux.conf exist, delete .tmux.conf.bak and relink"
+    rm -fr $HOME/.tmux.conf
     ln -s $HOME/.vim/tmuxrc/tmux.conf $HOME/.tmux.conf
 else
     echo "install .tmux.conf"
     ln -s $HOME/.vim/tmuxrc/tmux.conf $HOME/.tmux.conf
 fi
 
-if [ -f $HOME/.bash_aliases ];then
-    echo ".bash_aliases exist, rename to .bash_aliases.bak"
-    mv $HOME/.bash_aliases $HOME/.bash_aliases.bak
+if [ -L $HOME/.bash_aliases ];then
+    echo ".bash_aliases exist, delete .bash_aliases.bak and relink"
+    rm -fr $HOME/.bash_aliases
     ln -s $HOME/.vim/bash_aliases $HOME/.bash_aliases
 else
     echo "install .bash_aliases"
     ln -s $HOME/.vim/bash_aliases $HOME/.bash_aliases
 fi
-
+#fi
